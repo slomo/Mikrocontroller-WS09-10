b2ef953861d9b22380886911e3ca01109755ac9e
Robin Nehls <nehls@mi.fu-berlin.de>
2009-12-27 17:10:05 +0100

    Added comments to aufgabe10

--- src/aufgabe10.c
+++ src/aufgabe10.c
@@ -6,13 +6,14 @@
 #include "stdio.h"			// includes TI MSP430F1612 
 #include "SHT11.h"			// SHT11 Temperatur- und Feuchtesensor
 
+// Definitionen um das Ein- und Ausschalten von LEDs zu erleichtern
 #define RED					(0x01)
-#define YELLOW				(0x02)
 #define GREEN				(0x04)
 #define LED_OFF(led)	    (P4OUT |= led)    
 #define LED_ON(led)      	(P4OUT &= ~led)    
-#define LED_TOGGLE(led)  	(P4OUT ^=  led)
 
+// Funtion, welche die bereits vorhandene Funktion wait verwendet
+// um den Programmfluss zu um n Millisekunden zu verzoegern
 void delay(unsigned int time_mill) {	
 	unsigned int i;
 	for(i=0;i<=time_mill;++i){
@@ -21,25 +22,47 @@ void delay(unsigned int time_mill) {
 }
 
 void aufgabe10() {
-	//Wachund rauslassen
+	// Teiler für den ACLK Takt auf 8 setzen
 	BCSCTL1	|= DIVA0;
 	BCSCTL1	|= DIVA1;
+    
+    // Wachund rauslassen
+    // WDTCNTCL setzt den Watchdog Timer zurueck
+    // WDTSSEL wählt den ACLK Takt als Quelle fuer den Watchdogzaehler
+    // Bei dieser Operation werden WDTIS0/1 und WDTHOLD auf 0 gesetzt
+    // Das hat zur Folge, dass der Watchdog auf RESET nach 32768 Takten
+    // gestellt und aktiviert wird.
+    // Neue werte müssen in Verbindung mit dem Watchdog Passwort WDTPW
+    // übergeben werden sonst wird ein RESET ausgelöst
 	WDTCTL = (WDTPW+WDTCNTCL+WDTSSEL);
+    
 	if(P1IN & 0x03) {
 		LED_ON(GREEN);
 	}
 	
 	while(1){
+        // 500 msec warten dann rote und gruene LED ausschalten
 		delay(500);
 		LED_OFF(RED);
 		LED_OFF(GREEN);
 		
+        // solange einer der beiden Taster gedruekt wird
 		while(P1IN & 0x03) {
+            
+            // Watchdog zähler zuruecksetzen
+            // wird diese Zeile entfernt, loest der Watchdog nach ca.
+            // 8 sec einen RESET aus
 			WDTCTL = (WDTPW+WDTCNTCL+WDTSSEL);
+            
+            // eine kurze Verzoegerung um ein moegliches Prellen des
+            // Tasters abzufangen
 			delay(1);
 		}
 		
+        // Watchdog zähler zuruecksetzen
 		WDTCTL = (WDTPW+WDTCNTCL+WDTSSEL);
+        
+        // 500 msec warten dann rote und gruene LED ausschalten
 		delay(500);
 		LED_ON(RED);
 	}
