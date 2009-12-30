2c73530c5e74a2c5c069b6e5c78569f884834c57
Robin Nehls <nehls@mi.fu-berlin.de>
2009-12-30 08:56:56 +0100

    Added comments to aufgabe12 - need discussion

--- src/aufgabe12.c
+++ src/aufgabe12.c
@@ -1,33 +1,40 @@
-#include "msp430x16x.h"		// Systemdefinitionen von TI fur den MSP430F1612
-#include "init.h"			// Initialisierung des Mikrocontrollers
-#include "CC1100.h"			// CC1100 Funktransceiver
-#include "system.h"			// Systemfunktionen MSB430H
-#include "interrupts.h"		// ISR - Interrupt Service Routinen
-#include "stdio.h"			// includes TI MSP430F1612 
-#include "SHT11.h"			// SHT11 Temperatur- und Feuchtesensor
+#include "msp430x16x.h" // Systemdefinitionen von TI fur den MSP430F1612
+#include "init.h"       // Initialisierung des Mikrocontrollers
+#include "CC1100.h"     // CC1100 Funktransceiver
+#include "system.h"     // Systemfunktionen MSB430H
+#include "interrupts.h" // ISR - Interrupt Service Routinen
+#include "stdio.h"      // includes TI MSP430F1612
+#include "SHT11.h"      // SHT11 Temperatur- und Feuchtesensor
 
-#define RED					(0x01)
-#define YELLOW				(0x02)
-#define GREEN				(0x04)
-#define LED_OFF(led)	    (P4OUT |= led)    
-#define LED_ON(led)      	(P4OUT &= ~led)    
-#define LED_TOGGLE(led)  	(P4OUT ^=  led)
+// Definitionen um das Ein- und Ausschalten von LEDs zu erleichtern
+#define RED             (0x01)
+#define LED_OFF(led)    (P4OUT |= led)
+#define LED_ON(led)     (P4OUT &= ~led)
 
-void delay(unsigned int time_mill) {	
-	unsigned int i;
-	for(i=0;i<=time_mill;++i){
-		wait(100);
-	}
+// Funtion, welche die bereits vorhandene Funktion wait verwendet
+// um den Programmfluss zu um n Millisekunden zu verzoegern
+void delay(unsigned int time_mill) {
+    unsigned int i;
+    for(i=0;i<=time_mill;++i){
+        wait(100);
+    }
 }
 
 void aufgabe12() {
-	_bis_SR_register(GIE);
-	P1IE |= 0x02;
-	P1IES &= ~0x00;
-	while(1){
-		delay(500);
-		LED_OFF(RED);
-		delay(500);
-		LED_ON(RED);
-	}
+    // Interupts erlauben
+    _bis_SR_register(GIE);
+    // Interupts fuer den Taster an Port 1.1 aktivieren
+    P1IE |= 0x02;
+    // IMHO macht das keinen Sinn
+    // wenn man etwas auschlieslich mit einsen verundet ist das Ergebnis
+    // das selbe
+    // TODO: klaehren was das soll
+    P1IES &= ~0x00;
+    while(1){
+        // Rote LED alle 500ms aus bzw. einschalten
+        delay(500);
+        LED_OFF(RED);
+        delay(500);
+        LED_ON(RED);
+    }
 }
--- src/interrupts_aufgabe12.c
+++ src/interrupts_aufgabe12.c
@@ -1,230 +1,45 @@
-#include "msp430x16x.h"
-#include "interrupts.h"
-#include "system.h"
-#include "CC1100.h"
-#include "stdio.h"
-#include "string.h"
+#include "msp430x16x.h" // Systemdefinitionen von TI fur den MSP430F1612
+#include "interrupts.h" // ISR - Interrupt Service Routinen
+#include "system.h"     // Systemfunktionen MSB430H
+#include "CC1100.h"     // CC1100 Funktransceiver
+#include "stdio.h"      // includes TI MSP430F1612 
 
-	
+
 //Zuweisung der Interruptvektoren auf die ISR Behandlungsroutinen
-//Bei Bedarf fur den benutzten Interrupt die Auskommentierung entfernen 
-//Die hochste Prioritat hat der int15
+PORT1_ISR(ISR_Port1)
 
-//______Interruptquelle_______________INTNR_|_ADR____|_Funktion_________|
-//DACDMA_ISR(ISR_DACDMA)         	//int00 | 0xFFE0 | DAC12 / DMA		|
-PORT2_ISR(ISR_Port2)             	//int01 | 0xFFE2 | Port2			|
-//USART1TX_ISR(ISR_USART1_Transmit)	//int02 | 0xFFE4 | USART1 Transmit 	|
-//USART1RX_ISR(ISR_USART1_Receive)  //int03 | 0xFFE6 | USART1 Receive	|
-PORT1_ISR(ISR_Port1)                //int04 | 0xFFE8 | Port1 			|
-//TIMERA1_ISR(ISR_Timer_A)          //int05 | 0xFFEA | TimerA CC1-2,TA	|
-//TIMERA0_ISR(ISR_Timer_A_CCR0)     //int06 | 0xFFEC | TimerA CC0 		|
-//ADC12_ISR(ISR_ADC12)             	//int07 | 0xFFEE | ADC				|
-//USART0TX_ISR(ISR_USART0_Transmit) //int08 | 0xFFF0 | USART0 Transmit	|
-//USART0RX_ISR(ISR_USART0_Receive)  //int09 | 0xFFF2 | USART0 Receive	|
-//WDT_ISR(ISR_Watchdog)             //int10 | 0xFFF4 | Watchdog Timer	|
-//COMPARATORA_ISR(ISR_Comparator_A) //int11 | 0xFFF6 | Comparator A		|
-//TIMERB1_ISR(ISR_Timer_B)          //int12 | 0xFFF8 | Timer B CC1-6,TB	|
-//TIMERB0_ISR(ISR_Timer_B_CCR0)     //int13 | 0xFFFA | Timer B CC0 		|
-//=======================================================================
-//NMI_ISR (ISR_NMI)					//int14 | 0xFFFC | Non-maskable		|						|
-//									//int15 | 0xFFFE | Reset 			|
-//=======================================================================
-//
-
-//==============================================================
-// Implementierung der ISR Routinen 
-// Achtung! Code in der ISR nicht zu lang machen,
-// da sonst Timing oder Stack Probleme drohen.
-// Fur die benotigten ISR die Auskommentierungen entfernen
-// und eigenen Code fur die jeweilige ISR einfugen
-//==============================================================
-
-
-//==============================================================
-//===INT:00====ADR:FFE0====DAC12 DMA ===========================
-//==============================================================
-//__interrupt void ISR_DACDMA (void) {
-//  ...hier den Code der ISR einfugen
-//}
-//==============================================================
-
-		
-//==============================================================
-//===INT:01====ADR:FFE2====PORT2================================
-//==============================================================
-__interrupt void ISR_Port2 (void) {
-	char res ;			// CRC Check 
-	if (P2IFG & 0x01)	// Check P2IFG Bit P2.0 - CC1100 Rx Packet
-		{ 
-		CLEAR(P2IFG, 0x01);
-		res = receivePacket(); 	// CRC Ruckgabe 
-		if (res)				// wenn Packet OK ...
-			{
-			printPacket();		// Packet auf Terminal ausgeben
-			}
-		else
-			{	
-			spiStrobe(CC1100_SIDLE); 	// Switch to IDLE
-			spiStrobe(CC1100_SFRX);	 	// Flush the RX FIFO
-			}
-		}	
-	else
-		{
-		CLEAR(P2IFG, 0xFF);			 	// Clear all flags
-		}
-	spiStrobe(CC1100_SRX);			 	// Switch to RX Mode
-	}
-
-//==============================================================
-//===INT:02====ADR:FFE4====USART1 Transmit======================
-//==============================================================
-//__interrupt void Interruptroutine_USART1_Transmit (void) {
-//  ...hier den Code der ISR einfugen
-//}
-//==============================================================
-
-
-//==============================================================
-//===INT:03====ADR:FFE6====USART1 Receive=======================
-//==============================================================
-//__interrupt void ISR_USART1_Receive (void)
-//	{
-//	unsigned char Zeichen;
-//	IE2 &= ~URXIE1;			// Disable USART1 RX interrupt.
-//	if (UART1writePointer == 32) UART1writePointer=0;
-//	Zeichen = U1RXBUF;
-//	UART1buffer[UART1writePointer++] = Zeichen;
-//	UART1bufferLen++;
-//  	if (Zeichen != '\n')
-//  		{	
-//		U1TXBUF = Zeichen; 	// Echo
-//		} 
-//	else
-//		{
-//		//writestr(&UART1buffer,strlen(UART1buffer));
-//		UART1writePointer=0;
-//		}				
-//	IE2 |= URXIE1;    		// Enable USART1 RX interrupt. 
-//	}
-//==============================================================
-
-
-//==============================================================
-//===INT:04====ADR:FFE8====PORT1================================
-//==============================================================
-
+// Zaehler fuer die Anzahl der Tasterbetaetigungen
 int i=0;
 
 __interrupt void ISR_Port1 (void) { 
-	switch (P1IFG & 0x03) {
-		case 0x01 :  // rechter Taster gedruckt
-			i++;
-		  	if (i == 10) {
-		  		P4OUT ^= 0x04;
-		  		i=0;
-		  	}
-		  	wait(100);
-	  	break;
-		case 0x02 : // linker Taster gedruckt
-			P4OUT ^= 0x02;
-			P1IE ^= 0x01;
-			wait(100);
-		break;
+    switch (P1IFG & 0x03) {
+        case 0x01 :  // rechter Taster gedrueckt
+            // Zaehler fuer Tasterbetaetigungen erhoehen
+            i++;
+            // wenn der Zaehler 10 erreichet, gruene LED toggeln und
+            // Zahler auf 0 zuruecksetzen
+            if (i == 10) {
+                P4OUT ^= 0x04;
+                i=0;
+            }
+            // kurz warten um eventuelles Prellverhalten des Tasters
+            // abzufangen
+            wait(100);
+        break;
+        case 0x02 : // linker Taster gedruckt
+            // gelbe LED toggeln
+            P4OUT ^= 0x02;
+            // Interupts fuer den Taster an Port1.0 toggeln
+            P1IE ^= 0x01;
+            // kurz warten um eventuelles Prellverhalten des Tasters
+            // abzufangen
+            wait(100);
+        break;
 
-	}
-  		
-  	CLEAR(P1IFG, 0xFF); // Clear all flags
-  	
+    }
+
+    // Alle Interuptflags für Port 1 entfernen um eine Wiederholung
+    // der ISR, wegen gesetzem Interuptfalg, zu verhindern
+    CLEAR(P1IFG, 0xFF);
+   
 }
-//==============================================================
-
-
-//==============================================================
-//===INT:05====ADR:FFEA====Timer_A Capture/Compare 1?2==========
-//==============================================================
-//__interrupt void ISR_Timer_A (void) {
-//  ...hier den Code der ISR einfugen
-// }
-//==============================================================
-
-
-//==============================================================
-//===INT:06====ADR:FFEC****Timer_A Capture/Compare 0============
-//==============================================================
-//__interrupt void ISR_Timer_A_CCR0 (void) {
-//  ...hier den Code der ISR einfugen
-// }
-//==============================================================
-
-
-//==============================================================
-//===INT:07====ADR:FFEE====ADC12================================
-//==============================================================
-//__interrupt void ISR_ADC12 (void)
-//	{
-//  ...hier den Code der ISR einfugen
-//	}
-//==============================================================
-
-
-//==============================================================
-//===INT:08====ADR:FFF0====USART0 Transmit======================
-//==============================================================
-//__interrupt void ISR_USART0_Transmit (void) {
-//  ...hier den Code der ISR einfugen
-// }
-//==============================================================
-
-
-//==============================================================
-//===INT:9====ADR:FFF2====USART0 Empfang========================
-//==============================================================
-//__interrupt void ISR_USART0_Receive (void) {
-//  ...hier den Code der ISR einfugen
-// }
-//==============================================================
-
-
-//==============================================================
-//===INT:10====ADR:FFF4====Watchdog Timer=======================
-//==============================================================
-//__interrupt void ISR_Watchdog (void) {
-//  ...hier den Code der ISR einfugen
-// }
-//==============================================================
-
-
-//==============================================================
-//===INT:11====ADR:FFF6****Comparator_A=========================
-//==============================================================
-//__interrupt void ISR_Comparator_A (void) {
-//  ...hier den Code der ISR einfugen
-// }
-//==============================================================
-
-
-//==============================================================
-//===INT:12====ADR:FFF8====Timer_B Capture/Compare 1?6==========
-//==============================================================
-//__interrupt void ISR_Timer_B (void) { 
-//  ...hier den Code der ISR einfugen
-//}
-//==============================================================
-
-
-//==============================================================
-//===INT:13===ADR:FFFA====Timer_B Capture/Compare 0=============
-//==============================================================
-//__interrupt void ISR_Timer_B_CCR0 (void) { 
-//  ...hier den Code der ISR einfugen
-//}	
-//===============================================================
-
-
-//==============================================================
-//===INT:14===ADR:FFFC====NMI ==================================
-//==============================================================
-//__interrupt void ISR_NMI (void) { 
-//  ...hier den Code der ISR einfugen
-//}	
-//===============================================================
