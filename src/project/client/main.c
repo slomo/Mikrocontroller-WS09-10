a98ffdb2d18e662cd51b788bddb45dafeff931c5
Robin Nehls <nehls@mi.fu-berlin.de>
2010-02-27 18:20:40 +0100

    Added comments to the project

--- src/project/client/main.c
+++ src/project/client/main.c
@@ -1,6 +1,3 @@
-//=========================================================================
-//=== Startprojekt zum Mikroprozessorpraktikum WS0910 =====================
-//=========================================================================
 #include "msp430x16x.h"		// Systemdefinitionen von TI für den MSP430F1612
 #include "../init.h"			// Initialisierung des Mikrocontrollers
 #include "../CC1100.h"			// CC1100 Funktransceiver
@@ -10,60 +7,17 @@
 #include "../SHT11.h"			// SHT11 Temperatur- und Feuchtesensor
 #include "project.h"
 
-main(void);					//Haupprogramm
+main(void) {
+	// Initialisierung der Port Register
+	initPort();
+	// Dco Taktquelle aktivieren mit 7.3728MHz
+	DCO ();
+	// UART-RS232 mit 115.2kBit/s initialisieren
+	initUART1();
 
-//====Hauptprogramm=======================================================
-
-main(void)
-	{
-
-//===Hier sollten Variablen deklariert werden ============================
-	// unsigned char i = 0;
-
-//===Hier die notwendigen Initialisierungs Funktionen=====================
-	//====================================================================
-
-	initPort();				// Initialisierung der Port Register
-	//====================================================================
-	//== XT2() oder Dco() als Taktquelle einstellen
-	//== durch Ein- oder Auskommentieren
-	//== DCO ist bei LPM Einsatz bevorzugt muß zyklisch kalibriert werden
-	//== XT2 ist quarzstabil muß nicht zyklisch kalibriert werden
-	//  			
-	//XT2 ();				// XT2 Taktquelle aktivieren mit 7.3728MHz
-	DCO ();					// Dco Taktquelle aktivieren mit 7.3728MHz
-	//						   beachte DELTA
-	//					
-	//====================================================================
-	//flash_read(MEM_ID, &ID, 1);	// MSB430H ID aus dem INFOMEM lesen
-	//====================================================================
-	//init_timer();			// Init Timer für Sekundeninterrupt
-	initUART1();			// UART-RS232 mit 115.2kBit/s initialisieren
-	//====================================================================
-	//== Initialisierung des CC1100 Transceivers - bitte nicht verändern!
-	//initUART0_SPI();		// CC1100 SPI UART initalisieren
-	//initCC1100();			// CC1100 init und in RX Mode setzen
-	//initCC1100_IDLE();	// CC1100 init und in den IDLE Mode setzen
-	//initCC1100_POWERDOWN();	// CC1100 in den PowerDown Mode setzen
-	//setUid(0);			// CC1100 Adresse setzen, ID aus CC1100.c 
-	//====================================================================
-
-//===Hier die Endlosschleife quasi das Betriebssystem=====================
-	while(1)
-		{
+	while(1) {
 		project();
-		//augfgabe25();
-		// Hier den Code für die Lösung der Aufgabenstellung einfügen 
-		// am besten in der Form:
-		//
-		// Aufgabe1(); // Aufgabe1.h nicht vergessen!
-		//
-		// Aufgabex() sollte eigene Initialisierungsroutine enthalten und 
-		// eine eigene Endlosschleife 
-		
-		}	// Ende der Endlosschleife
-
-	}		// Ende Main
-//====Ende des Hauptprogramms ============================================
+	}
+}
 
 
--- src/project/server/project.c
+++ src/project/server/project.c
@@ -1,14 +1,14 @@
-#include "msp430x16x.h"        // Systemdefinitionen von TI für den MSP430F1612
-#include "../init.h"            // Initialisierung des Mikrocontrollers
-#include "../CC1100.h"            // CC1100 Funktransceiver
-#include "../system.h"            // Systemfunktionen MSB430H
-#include "interrupts.h"        // ISR - Interrupt Service Routinen
-#include "stdio.h"            // includes TI MSP430F1612 
-#include "../SHT11.h"            // SHT11 Temperatur- und Feuchtesensor
+#include "msp430x16x.h"	// Systemdefinitionen von TI für den MSP430F1612
+#include "../init.h"	// Initialisierung des Mikrocontrollers
+#include "../CC1100.h"	// CC1100 Funktransceiver
+#include "../system.h"	// Systemfunktionen MSB430H
+#include "interrupts.h"	// ISR - Interrupt Service Routinen
+#include "stdio.h"		// includes TI MSP430F1612 
+#include "../SHT11.h"	// SHT11 Temperatur- und Feuchtesensor
 #include "project.h"
 #include "math.h"
 
-
+// Arrays welche die Kordinaten der auszugebenden Punkte beinhalten
 int valuesX[ANTZ];
 int valuesY[ANTZ];
 
@@ -16,25 +16,34 @@ int i;
 
 volatile int ic=2;
 
+// Structs welche die Schlaeger beschreiben
 barstate left_bar;
 barstate right_bar;
+
 int life_left;	// Player 1 auf cahnnel 2
 int life_right; // Player 2 auf channel 3
 int running;
+
+// Struct welches den Zustand des Balls beschreibt
 ballstate ball;
 
+// Anzahl der Spieler
 int player;
+// Gibt an ob genug spieler Bereit sind fuer ein Spiel
 int start_game;
 
 char str[255];
 
-void delay(unsigned int time_mill) {	
+// Funtion, welche die bereits vorhandene Funktion wait verwendet
+// um den Programmfluss zu um n Millisekunden zu verzoegern
+void delay(unsigned int time_mill) {
 	unsigned int i;
 	for(i=0;i<=time_mill;++i){
 		wait(100);
 	}
 }
 
+// Defines um einige Berechungen lesbarer zu machen
 #define MAX(s1,s2) ((s1 > s2) ? s1 : s2) 
 #define MIN(s1,s2) ((s1 < s2) ? s1 : s2)
 #define GRAD(g) (((g) / 360.0) * 2.0 * M_PI)
@@ -42,30 +51,29 @@ void delay(unsigned int time_mill) {
 void project(){
 	int count;
 	
-    //DMA vorbereiten
-    DMACTL0 = DMA0TSEL_8 + DMA1TSEL_8;
-    DMACTL1 = ROUNDROBIN;
-    
-    DMA0CTL = DMASRCINCR_3 + DMADT_4 + DMAEN;
-    DMA0SZ = ANTZ;
-    DMA0SA = (unsigned int) valuesX;
-    DMA0DA = (unsigned int) &DAC12_0DAT;
-    
-    DMA1CTL = DMASRCINCR_3 + DMADT_4 + DMAEN;
-    DMA1SZ = ANTZ;
-    DMA1SA = (unsigned int) valuesY;
-    DMA1DA = (unsigned int) &DAC12_1DAT;
-    
-    //DAC vorbereiten
-    DAC12_1CTL = DAC12SREF0 + DAC12SREF1 + DAC12IR + DAC12AMP1 + DAC12ENC + DAC12LSEL0;
-    DAC12_0CTL = DAC12SREF0 + DAC12SREF1 + DAC12IR + DAC12AMP1 + DAC12ENC + DAC12LSEL0;
-    
-    //Timer und output
-    //P6SEL = 0xc0;
-    TBCTL     = MC_1 + TASSEL_2 + ID_3;
-    //TBCCTL0 = CCIE; 
-    TBCCR0     = 100;
+	// DMA vorbereiten
+	DMACTL0 = DMA0TSEL_8 + DMA1TSEL_8;
+	DMACTL1 = ROUNDROBIN;
 
+	DMA0CTL = DMASRCINCR_3 + DMADT_4 + DMAEN;
+	DMA0SZ = ANTZ;
+	DMA0SA = (unsigned int) valuesX;
+	DMA0DA = (unsigned int) &DAC12_0DAT;
+
+	DMA1CTL = DMASRCINCR_3 + DMADT_4 + DMAEN;
+	DMA1SZ = ANTZ;
+	DMA1SA = (unsigned int) valuesY;
+	DMA1DA = (unsigned int) &DAC12_1DAT;
+
+	// DAC vorbereiten
+	DAC12_1CTL = DAC12SREF0 + DAC12SREF1 + DAC12IR + DAC12AMP1 + DAC12ENC + DAC12LSEL0;
+	DAC12_0CTL = DAC12SREF0 + DAC12SREF1 + DAC12IR + DAC12AMP1 + DAC12ENC + DAC12LSEL0;
+
+	//Timer und Output einstellen
+	TBCTL = MC_1 + TASSEL_2 + ID_3;
+	TBCCR0 = 100;
+
+	// Interupts fuer Taster aktivieren
 	P1IE |= 0x03;
 	P1IES &= ~0x03;
     
@@ -74,141 +82,152 @@ void project(){
 	ADC12MCTL0 = SREF_0 + INCH_0 + EOS;
 	ADC12CTL1 = CONSEQ_1 + SHP;
 	ADC12CTL0 = ADC12ON + ENC + ADC12SC + MSC;
-	
+	//GS1 und GS2 (empfindlichkeit) einstellen
 	P5DIR |= (64+32+16);
-	P5OUT &= ~(32 + 16); //GS1 und GS2 (empfindlichkeit) einstellen
-	P5OUT |= 64;			//Wake up, wake up, wake up now... so tierd of wa~i~ting ...*sing*
+	P5OUT &= ~(32 + 16);
+	P5OUT |= 64; // Wake up
 
-	ADC12IE = 0x01; // AD-Wandler Intterrup enable
+	ADC12IE = 0x01; // AD-Wandler Interupt einschalten
 	_bis_SR_register(GIE); //Interrupts zulassen
 
+	// ENC Toggle um eine Wandlung zu starten
 	ADC12CTL0 |=  ADC12SC;
 	ADC12CTL0 &= ~ADC12SC;    
-    
-    DAC12_1DAT=valuesY[0];
-    DAC12_0DAT=valuesX[0];
 
+	// Werte Initialisieren
+	DAC12_1DAT=valuesY[0];
+	DAC12_0DAT=valuesX[0];
 	player = 0;
 	start_game = 0;
-	
+
 	// Funk vorbereiten
 	initUART0_SPI();
 	initCC1100();
 	setUid(0);
 	switchFreq(7);
-	
+
 	// here be pong
-    init(&ball);
-  	   	
-    while(1) {
-    	int dir;
-   	
-    	if (running) {
-	        if((dir = next(&ball, left_bar, right_bar)) != IN) {
-	        	if ((life_left < 0 || life_right < 0) && player == 2) {
-	        		running = 0;
-	        		LED_OFF(GREEN);
-	        		LED_ON(RED);
-	   				// TODO WIN und FAIL bestimmen
-	        	}
-	        	else {
-	        		char buf[2] = {'L','0'};
-	    	    	reset_ball(&ball, dir);
-	    	    	
-	    	    	// verbleibende Leben senden
+	init(&ball);
+
+	while(1) {
+		int dir;
+
+		if (running) {
+			// naechsten Punkt fuer den Ball berechnen
+			if((dir = next(&ball, left_bar, right_bar)) != IN) {
+				// Wenn der Ball ausserhalb des spielfelds ist und
+				// ein spieler keine Leben mehr hat, spiel anhalten
+				if ((life_left < 0 || life_right < 0) && player == 2) {
+					running = 0;
+					LED_OFF(GREEN);
+					LED_ON(RED);
+					// TODO WIN und FAIL bestimmen
+				}
+			// Andernfalls den Spielern die Anzahl ihrer Leben
+			// zuschicken und den Ball reseten
+			else {
+				char buf[2] = {'L','0'};
+					reset_ball(&ball, dir);
+					
+					// verbleibende Leben senden
 					buf[1] = '0' + life_left;
 					sendPacket(2, 0, buf, 2);
 					buf[1] = '0' + life_right;
 					sendPacket(3, 0, buf, 2);
-	        	}
-        	}
+				}
+			}
 
+			// Wenn noch nicht genug Spieler da sind, zeige 2 "KIs" die
+			// perfekt Pong spielen
 			if (player < 2) {
-		        left_bar = MAX(0 + BARLENGTH/2.0, MIN(FIELD_Y - BARLENGTH/2.0, ball.y));
-		        right_bar = MAX(0 + BARLENGTH/2.0, MIN(FIELD_Y - BARLENGTH/2.0, ball.y));
+				left_bar = MAX(0 + BARLENGTH/2.0, MIN(FIELD_Y - BARLENGTH/2.0, ball.y));
+				right_bar = MAX(0 + BARLENGTH/2.0, MIN(FIELD_Y - BARLENGTH/2.0, ball.y));
 			}
 			
-	        generate_array(&ball, left_bar, right_bar);
-	        
-    	}
+			// Erzeuge das Array mit den zu malenden Bildpunkten neu
+			generate_array(&ball, left_bar, right_bar);
 
-        if (start_game) {
-        	running = 0;
-        	start_game_now();
-        }
-        
-        if (player < 2) {
-	        // jedes hundertste Mal
-    	    if ((++count % 100) == 0) {
-        		count = 0;
-        	
-	        	// W = WAITING_FOR_PLAYERS
-    	    	sendPacket(1, 0, "W", 1);
-        		LED_TOGGLE(YELLOW);
-    	    }
-        }
-    }    
+		}
+
+		// Auf los gehts los
+		if (start_game) {
+			running = 0;
+			start_game_now();
+		}
+
+		// Wenn noch nicht genug spieler da sind alle 100 druchlaeufe
+		// ein Wainting for players Packet senden
+		if (player < 2) {
+			// jedes hundertste Mal
+			if ((++count % 100) == 0) {
+				count = 0;
+
+				// W = WAITING_FOR_PLAYERS
+				sendPacket(1, 0, "W", 1);
+				LED_TOGGLE(YELLOW);
+			}
+		}
+	}
 }
 
-/**
- * @return Position der nächsten Angabe.
- */
+// Diese Funktion berechnet die naechste Position des Balls
 int next(ballstate *ball, barstate bar_left, barstate bar_right)
 {
-        // move ball
         float m_x, m_y;
         
-        // calulcate the movement vectors
+        // Richtungsvektoren der Bewegung berechnen
         m_x = ball->speed * cos(ball->angle);
         m_y = ball->speed * sin(ball->angle);
         
-        // add movement vectros to current position
+        // Vektoren zur aktuellen Position addieren
         ball->x += m_x;
         ball->y += m_y;
         
-        // check for collisions
-        // ball has reached the borders
+        // Kollisionskontrolle
+        // Ball hat einen Rand des Spielfeld erreicht
+        // Refelktion berechnen
         if ((ball->y <= 0) || (ball->y >= FIELD_Y)) {
             ball->angle = normalize_radiant(2*M_PI - ball->angle);
             
             if (ball->y >= FIELD_Y) {
-                // ball crossed border at yMax
+                // Ball trifft Wand bei yMax
                 ball->y = 2 * FIELD_Y - ball->y;
             }
             else {
-                // ball crossed border at yMin
+                // Ball trifft Wand bei yMin
                 ball->y = fabs(ball->y);
             }
-            
+            // Geschwindikeit des Balls erhoehen (mehr Spass)
             ball->speed += 0.5;
         }
         
-        // ball has passed bar layer
+        // Ball ist auf einer Spielerseite angekommen
         if(ball->x <= 0 || ball->x >= FIELD_X){
 
+			// Variable zur bestimmung Wo der Schlaeger getroffen wurde
+			// um den Reflektionswinkel davon abhaenging zu machen
             float bar_position;
             
             if (ball->x <= 0) {
-                // calculate if 
 	            float collision_point_y = fabs(tan(ball->angle) * (ball->x - m_x) + (ball->y - m_y));
                 bar_position = collision_point_y - bar_left;
 
-                // ball crossed border at xMin
+                // Ball trifft Ebene bei xMin
                 ball->x = fabs(ball->x);
             }
             else {
-                // calculate if 
 	            float collision_point_y = fabs(tan(ball->angle) * (FIELD_X - ball->x - m_x) + (ball->y - m_y));
                 bar_position = bar_right - collision_point_y;
 
-                // ball crossed border at xMax
+                // Ball trifft Ebene bei xMax
                 ball->x = 2 * FIELD_X - ball->x;
             }
 
             if (fabs(bar_position) <= (BARLENGTH/2.0)) {
                 ball->angle = normalize_radiant((M_PI - ball->angle) + (bar_position / (BARLENGTH/2.0)) * GRAD(30.0));
-                		
-				// Winkel nicht zu steil
+
+				// Dafuer sorgen das der Reflektionswinkel nicht zu
+				// steil wird
 				if (ball->angle > GRAD(70.0) && ball->angle < GRAD(110.0)) {
 					if (ball->angle > GRAD(90.0)) {
 						ball->angle = GRAD(110.0);
@@ -225,21 +244,20 @@ int next(ballstate *ball, barstate bar_left, barstate bar_right)
 						ball->angle = GRAD(250.0);
 					}
 				}
-								
-                //Use the speedup feature later when averything works
+
+                // Geschwindigkeit des Ball erhoehen und getroffen
+                // zurueckgeben wenn der Ball getroffen wurde
                 ball->speed += 1;
                 return IN;
             }
             
+            // Entscheiden welche Seite Schuld war im Falle, dass der
+            // Ball nicht getroffen wird
             if (ball->x > (FIELD_X / 2.0)) {
             	life_left--;
-            	sprintf(str, "Left %d\n", life_left);
-            	writestr(str);
             	return LEFT;
             }
 
-          	sprintf(str, "Right %d\n", life_right);
-           	writestr(str);
             life_right--;
             return RIGHT;
         }
@@ -247,27 +265,33 @@ int next(ballstate *ball, barstate bar_left, barstate bar_right)
         return IN;
 }
 
+// Funktion erstellt die Arrays fuer die Ausgabe auf dem Oszi
 void generate_array(ballstate *ball, barstate bar_left, barstate bar_right)
 {
     int i, count;
+    // Punkte fuer den Ball erstellen
     for(i=0;i<BALL_POINTS;i++){
         valuesX[i] = (int)(ball->x * RES_X/FIELD_X);
         valuesY[i] = (int)(ball->y * RES_Y/FIELD_Y);
     }
     
+    // Buchfuehrung
     count = ((ANTZ-BALL_POINTS) / 2);
+    
+    // Linken schlaeger malen
     for (i = 1; i < count + 1; i++) {
         valuesX[i + BALL_POINTS-1] = 0;
         valuesY[i + BALL_POINTS-1] = (int)(((bar_left - BARLENGTH/2.0) + (BARLENGTH / count) * i) * RES_Y/FIELD_Y);
     }
     
-    
+    //Rechten schlaeger malen
     for (i = count + 1; i <= ANTZ - BALL_POINTS; i++) {
         valuesX[i + BALL_POINTS-1] = (int)RES_X;
         valuesY[i + BALL_POINTS-1] = (int)(((bar_right - BARLENGTH/2.0) + (BARLENGTH / count) * (i-count)) * RES_Y/FIELD_Y);
     }
 }
 
+// Funktion die alle werte fuer ein Spiel intialisiert
 void init(ballstate *ball)
 {
 	left_bar = FIELD_Y / 2;
@@ -285,6 +309,8 @@ void init(ballstate *ball)
 	running = 1;
 }
 
+// Funktion die den Ball zuruecksetzt und mit zufaelligenm Wikel starten
+// laesst
 void reset_ball(ballstate *ball, int direction)
 {
     ball->x = FIELD_X / 2 + (FIELD_X / 4.0 * -1.0 * direction);
@@ -299,6 +325,8 @@ void reset_ball(ballstate *ball, int direction)
     }
 }
 
+// Funktion welche einen Rad annimmt und zwischen 0 und 2*Pi
+// normalisiert
 float normalize_radiant(float source)
 {
 	float new;
@@ -315,6 +343,7 @@ float normalize_radiant(float source)
 	return new;
 }
 
+//Funktion startet ein neues Spiel
 void start_game_now()
 {
 	char buf[2] = {'L','0'};
@@ -325,6 +354,7 @@ void start_game_now()
 	life_left=3;
 	life_right=3;
 	
+	// Ampelssequenz
 	delay(1000);
 	
 	LED_ON(RED);
@@ -344,6 +374,7 @@ void start_game_now()
 	LED_OFF(RED);
 	LED_ON(GREEN);
 	
+	//Spiel starten
 	running = 1;
 	start_game = 0;
 	
